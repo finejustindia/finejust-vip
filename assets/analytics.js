3ccab327f8d228576a5d9ebbc97a58a72330afe3
finejustindia <finejustindia@gmail.com>
2026-08-16 23:43:21 +0530

    Add files via upload

--- assets/analytics.js
+++ assets/analytics.js
@@ -1,14 +1,12 @@
-/**
- * Google Analytics 4 (GA4) Global Site Tag
- * Brand: Finejust.vip
- * Production Measurement ID: G-HXVBES3BJF
- */
 window.dataLayer = window.dataLayer || [];
+
 function gtag(){dataLayer.push(arguments);}
+
 gtag('js', new Date());
 
-// Configuration with performance and privacy optimizations
 gtag('config', 'G-HXVBES3BJF', {
-  'anonymize_ip': true,
-  'cookie_flags': 'SameSite=None;Secure'
+  anonymize_ip: true,
+  page_title: document.title,
+  page_location: window.location.href,
+  send_page_view: true
 });
